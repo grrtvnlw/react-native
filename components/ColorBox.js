c55752462f4457a85fb644bdf55dccb37fe29298
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2023-02-04 11:25:10 -0500

    init

---
+++ components/ColorBox.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const ColorBox = ({colorName, hexCode}) => {
+  const colorStyle = {
+    backgroundColor: hexCode,
+  };
+  const textColor = {
+    color:
+      parseInt(hexCode.replace('#', ''), 16) > 0xffffff / 1.1
+        ? 'black'
+        : 'white',
+  };
+  return (
+    <View style={[styles.box, colorStyle]}>
+      <Text style={[styles.text, textColor]}>
+        {colorName}: {hexCode}
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  box: {
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.3,
+    shadowRadius: 1,
+    elevation: 2,
+  },
+  text: {color: 'white', fontWeight: 'bold'},
+});
+
+export default ColorBox;
---
+++ screens/ColorPalette.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {FlatList, Text, StyleSheet} from 'react-native';
+
+import ColorBox from '../components/ColorBox';
+
+const ColorPalette = ({route}) => {
+  const {colors, paletteName} = route.params;
+  return (
+    <FlatList
+      style={styles.container}
+      data={colors}
+      keyExtractor={item => item.colorName}
+      renderItem={({item}) => (
+        <ColorBox colorName={item.colorName} hexCode={item.hexCode} />
+      )}
+      ListHeaderComponent={<Text style={styles.text}>{paletteName}</Text>}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 10,
+    paddingTop: 10,
+    backgroundColor: 'white',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+});
+
+export default ColorPalette;
